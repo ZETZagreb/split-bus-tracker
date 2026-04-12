600d4843d626a3e4493242df67e11d6b810fd8e2
ZETZagreb <maric.leon2001@gmail.com>
2026-04-12 23:22:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ from supabase import create_client, Client
 
 app = Flask(__name__, template_folder='templates')
 
-# Supabase podaci
 SUPABASE_URL = "https://ohxghzlbdflyqjatcwcb.supabase.co"
 SUPABASE_KEY = "sb_publishable_hBKMq44_LWLCjlO_PfKQ9Q_yB-mZVDO"
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
@@ -23,20 +22,27 @@ def get_buses():
         r = requests.get(url, headers=headers, timeout=10)
         data = r.json()
         vehicles = data.get("vehicles", [])
-        
         now_date = datetime.now().strftime("%d.%m.%Y.")
         now_time = datetime.now().strftime("%H:%M")
         
         for bus in vehicles:
+            line_clean = str(bus.get("name", "")).replace("Linija ", "").strip()
+            trip_id = bus.get("tripId") or bus.get("blockId") or "Nije prijavljen"
+            sch_dep = bus.get("scheduledDeparture") or "---"
+            direction = bus.get("destinationName") or "U prometu"
+
             try:
                 supabase.table("bus_logs").insert({
                     "garage_num": str(bus.get("garageNumber")),
-                    "line": str(bus.get("name", "")).replace("Linija ", ""),
+                    "line": line_clean,
                     "reg": str(bus.get("registrationNumber")),
                     "date": now_date,
                     "time": now_time,
                     "lat": bus.get("latitude"),
-                    "lon": bus.get("longitude")
+                    "lon": bus.get("longitude"),
+                    "trip_id": str(trip_id),
+                    "scheduled_departure_time": str(sch_dep),
+                    "direction": str(direction)
                 }).execute()
             except:
                 continue
@@ -48,20 +54,8 @@ def get_buses():
 @app.route('/api/full_history/<garage_num>')
 def get_full_history(garage_num):
     try:
-        response = supabase.table("bus_logs") \
-            .select("line, date, time, reg") \
-            .eq("garage_num", garage_num) \
-            .order("id", desc=True) \
-            .limit(100) \
-            .execute()
-        
-        history = []
-        last_line = ""
-        for r in response.data:
-            if r['line'] != last_line:
-                history.append(r)
-                last_line = r['line']
-        return jsonify(history)
+        res = supabase.table("bus_logs").select("*").eq("garage_num", garage_num).order("id", desc=True).limit(100).execute()
+        return jsonify(res.data)
     except:
         return jsonify([])
 
